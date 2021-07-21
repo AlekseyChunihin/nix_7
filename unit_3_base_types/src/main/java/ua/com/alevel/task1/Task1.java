45d8fbc6a3d0e08610ef9b27d2502f5927a7a541
Aleksey Chunihin <alexey3322179@gmail.com>
2021-07-21 13:56:56 +0300

    fix Task1.java, Task2.java, unit3_start.bat

--- unit_3_base_types/src/main/java/ua/com/alevel/task1/Task1.java
+++ unit_3_base_types/src/main/java/ua/com/alevel/task1/Task1.java
@@ -9,13 +9,14 @@ public class Task1 {
     public static int sumOfDigitsInString(String string) {
         int digitCode = 48, sum = 0;
         boolean IsNumberEntered = false;
+        char zero = '0', nine = '9';
         for (int i = 0; i < string.length(); i++) {
-            if (string.charAt(i) >= '0' && string.charAt(i) <= '9') {
+            if (string.charAt(i) >= zero && string.charAt(i) <= nine) {
                 IsNumberEntered = true;
-                while (digitCode <= '9') {
+                while (digitCode <= nine) {
                     if (string.charAt(i) == digitCode) {
                         sum += conversionCharDigitToIntDigit(string.charAt(i));
-                        digitCode = '0';
+                        digitCode = zero;
                         break;
                     }
                     digitCode++;
