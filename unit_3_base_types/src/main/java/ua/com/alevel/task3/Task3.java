003bc6b44da845e6facb15cb946d747fd921bf23
Aleksey Chunihin <alexey3322179@gmail.com>
2021-07-27 11:58:46 +0300

    moved variables into constants

--- unit_3_base_types/src/main/java/ua/com/alevel/task3/Task3.java
+++ unit_3_base_types/src/main/java/ua/com/alevel/task3/Task3.java
@@ -2,8 +2,13 @@ package ua.com.alevel.task3;
 
 public class Task3 {
 
+    private static final int smallBreak = 5;
+    private static final int bigBreak = 15;
+    private static final int lessonDuration = 45;
+
     public static void lessonEndTime(int lessonNumber) {
-        int hour, minutes, smallBreak = 5, bigBreak = 15, lessonDuration = 45;
+
+        int hour, minutes;
         hour = lessonNumber * lessonDuration + lessonNumber / 2 * smallBreak + (lessonNumber - 1) / 2 * bigBreak;
         minutes = hour % 60;
         System.out.println("End time " + lessonNumber + " of lesson: " + (9 + hour / 60) + " hours " + minutes + " minutes");
