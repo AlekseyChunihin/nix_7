3385d16109ef00e95d2c68f3410d4633c163c5ab
Aleksey Chunihin <alexey3322179@gmail.com>
2021-08-03 13:51:56 +0300

    update .bat file

--- unit_5_strings/unit5_start.bat
+++ unit_5_strings/unit5_start.bat
@@ -1,4 +1,7 @@
 chcp 1251
+cd ../reversestring
+call mvn clean package install
+cd ../unit_5_strings
 call mvn clean install
 echo -----------------------------------------------------------------------------------
 call java -jar target\unit_5_strings.jar
