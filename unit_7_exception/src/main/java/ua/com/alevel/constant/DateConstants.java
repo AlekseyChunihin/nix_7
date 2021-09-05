734ced0f70d46ef2851aeb3ed50ff2cc471957ca
Aleksey Chunihin <alexey3322179@gmail.com>
2021-09-05 20:29:34 +0300

    fix constants

--- unit_7_exception/src/main/java/ua/com/alevel/constant/DateConstants.java
+++ unit_7_exception/src/main/java/ua/com/alevel/constant/DateConstants.java
@@ -11,15 +11,12 @@ public class DateConstants {
     public static final int[] MONTH_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     public static final String[] MONTHS_NAMES = {"January", "February", "March", "April", "May", "June", "July",
             "August", "September", "October", "November", "December"};
-
     public static final double MILLISECOND = 1;
     public static final double SECOND = MILLISECOND / 1000;
     public static final double MINUTE = SECOND / 60;
     public static final double HOUR = MINUTE / 60;
     public static final double DAY = HOUR / 24;
-
     public static final long YEAR = 31536000000L;
-    public static final long LEAP_YEAR = 31622400000L;
 
 
 }
